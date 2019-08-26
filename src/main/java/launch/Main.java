996e47c95434c7a9f282c9694433a9129e4b731b
chbu <chbu@dtu.dk>
2019-08-26 15:44:25 +0200

    Initial commit

---
+++ src/main/java/launch/Main.java
@@ -0,0 +1,34 @@
+package launch;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.WebResourceRoot;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.catalina.webresources.DirResourceSet;
+import org.apache.catalina.webresources.StandardRoot;
+
+import java.io.File;
+
+public class Main {
+
+    private static Tomcat tomcat;
+
+    public static void main(String[] args) throws LifecycleException {
+        tomcat = new Tomcat();
+        tomcat.setBaseDir("temp");
+        tomcat.setPort(8080);
+        tomcat.getConnector();
+
+        Context ctx = tomcat.addWebapp("/", new File("src/main/webapp").getAbsolutePath());
+
+        File additionWebInfClasses = new File("target/classes");
+        WebResourceRoot resources = new StandardRoot(ctx);
+        resources.addPreResources(new DirResourceSet(resources, "/WEB-INF/classes",
+                additionWebInfClasses.getAbsolutePath(), "/"));
+        ctx.setResources(resources);
+
+        tomcat.start();
+
+        tomcat.getServer().await();
+    }
+}
